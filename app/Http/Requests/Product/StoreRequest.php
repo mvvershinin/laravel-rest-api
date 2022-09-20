c2295324efa8ba09f9150017992559aba8b5c6e6
Mikhail Vershinin <mvvershinin@gmail.com>
2022-09-20 17:08:44 +0700

    Product resource fix

--- app/Http/Requests/Product/StoreRequest.php
+++ app/Http/Requests/Product/StoreRequest.php
@@ -28,10 +28,10 @@ class StoreRequest extends FormRequest
 
         return [
             'eid' => 'integer|required',
-            'title' => 'string|min:5|max:244',
-            'price' => 'numeric|min:1|max:20000',
+            'title' => 'string|required|min:5|max:244',
+            'price' => 'numeric|required|min:1|max:20000',
             'categories_ids' => 'required|array',
-            'categories_ids.*' => 'integer|exists:'.$categoriesRepository->getClassName().',id'
+            'categories_ids.*' => 'integer|required|exists:'.$categoriesRepository->getClassName().',id'
         ];
     }
 }
